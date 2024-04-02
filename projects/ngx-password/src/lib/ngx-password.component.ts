3ec9367a351361a4dbb4cf5d5a794a02acfcedb0
Brandon McCray <bradmccray89@gmail.com>
2024-04-01 22:00:33 -0400

    initial commit

---
+++ projects/ngx-password/src/lib/ngx-password.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-password',
+  templateUrl: './ngx-password.component.html',
+  styleUrls: ['./ngx-password.component.scss'],
+})
+export class NgxPasswordComponent {}
